50c1c5bad66a659b4b96cfa9ba8d6187817b4858
jermaine <jblearn2023@gmail.com>
2024-01-21 10:31:42 +0000

    add files

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+console.log("test")
+
+// Create a header/hero area that welcomes users to the application and displays the current time and date using day.js with `setInterval()`.
+
+// 2. Create a Bootstrap card component explaining the instructions of how to use the app and a button to open a [Bootstrap modal dialog](https://getbootstrap.com/docs/5.3/components/modal/#how-it-works).
+
+// 3. The modal should contain a form asking users to fill in the following data:
+
+//     * The name of the project
+
+//     * The type of project (use a `<select>` drop-down)
+
+//     * The hourly wage for the project
+
+//     * The due date for the project (use jQuery UI's datepicker with a minimum date setting in place)
+
+// 4. Include a Bootstrap table that the project's information can be printed to with columns for the following data:
+
+//     * Project name
+
+//     * Project type
+
+//     * Hourly wage
+
+//     * Due date
+
+//     * Days until the due date (use day.js to calculate)
+
+//     * Estimated total earned (hourly wage at 8 hours per day multiplied by the number of days until the due date)
